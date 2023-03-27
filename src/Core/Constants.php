b805d177c78fef4add589c1eb17cc2184b83b6c8
NadeauRaphael <raphynadeau@hotmail.com>
2023-03-27 12:46:09 -0400

    Change shipping cost

--- src/Core/Constants.php
+++ src/Core/Constants.php
@@ -4,7 +4,7 @@ namespace App\Core;
 
 class Constants
 {
-    public const SHIPPING_COST = 20.00;
+    public const SHIPPING_COST = 99.99;
     public const TVQ = 0.05;
     public const TVS = 0.0975;
 }
