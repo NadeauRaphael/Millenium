888bbe1fb5f469f5ebf5994e2d51641eaa2f2d78
NadeauRaphael <raphynadeau@hotmail.com>
2023-03-27 12:31:15 -0400

    bug fixing

--- src/Entity/Cart.php
+++ src/Entity/Cart.php
@@ -14,21 +14,6 @@ class Cart
     public function add($product, $quantity, $price): bool
     {
         $purchase = new Purchase($product, $quantity, $price);
-        $isInStock = $this->addQuantity($product);
-        // If it a new purchase check if we add at least one in stock
-        if($purchase->getProduct()->getStockQuantity() > 0 && $isInStock){
-            $this->purchases[] = $purchase;
-            $isInStock = true;
-        }else{
-            $isInStock = false;
-        }
-        return $isInStock;
-    }
-
-    public function addQuantity($product): bool
-    {
-        // Loop in the purchases to see if the product is already in the cart, 
-        // if yes then adding one quantity to the purchase quantity
         foreach ($this->purchases as $testPurchase) {
             if ($testPurchase->getProduct()->getIdProduct() == $product->getIdProduct()) {
                 $newQuantity = $testPurchase->getQuantity() + 1;
@@ -36,19 +21,27 @@ class Cart
                 if ($testPurchase->getProduct()->getStockQuantity() >= $newQuantity) {
                     $testPurchase->update($newQuantity);
                     return true;
-                } else return false;
+                }
+                else{return false;}
             }
         }
-        return true;
+        // If it a new purchase check if we add at least one in stock
+        if($purchase->getProduct()->getStockQuantity() > 0){
+            $this->purchases[] = $purchase;
+            return true;
+        }else{
+             return false;
+        }
     }
 
     public function update($newPurchases):bool
     {
+        $stock = false;
         if (count($this->purchases) > 0) {
-            $quantity = $newPurchases["quantity"];
-
+            $quantity = $newPurchases["quantity"]; 
             foreach ($this->purchases as $key => $purchase) {
                 $newQuantity = $quantity[$key];
+                var_dump($key);
                 // test if the quantity is zero, if yes then delete the product from the cart
                 if ($newQuantity == 0) {
                     $this->delete($key);
@@ -58,13 +51,14 @@ class Cart
                 else {
                     if($purchase -> getProduct() -> getStockQuantity() >= $newQuantity){
                         $purchase->update($newQuantity);
-                        return true;
+                        $stock = true;
                     }
                     else{
-                        return false;
+                        $stock = false;
                     }
                 }
             }
+            return $stock;
         }
     }
     public function getPurchases()
