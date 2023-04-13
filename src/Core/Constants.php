87d58ea1a0716709e2d2e1f031985e3d3dc21442
NadeauRaphael <raphynadeau@hotmail.com>
2023-04-13 15:19:03 -0400

    yannick correction issue 11

--- src/Core/Constants.php
+++ src/Core/Constants.php
@@ -5,6 +5,6 @@ namespace App\Core;
 class Constants
 {
     public const SHIPPING_COST = 99.99;
-    public const TVQ = 0.05;
-    public const TVS = 0.0975;
+    public const TVQ = 5/100;
+    public const TVS = 9.975/100;
 }
